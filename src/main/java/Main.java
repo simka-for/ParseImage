6a08b35f06372d7d549df83fcb72c94e4c6d9077
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-02-11 17:31:28 +0300

    new downloadMetod

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,50 @@
-import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Main {
+
+    public static  String output = "data/image/";
+    public static File outFile = new File("data/image");
+
     public static void main(String[] args) {
 
-        String pathHtml = parseFile("data/code.html");
-        File output = new File("data/image");
+        String pathHtml = parseFile();
 
         Document doc = Jsoup.parse(pathHtml);
         Elements img = doc.getElementsByTag("img");
         ArrayList<String> src = new ArrayList<>();
         img.forEach(element -> src.add(element.absUrl("src")));
-        for (String urls : src){
-            try {
-                downloadImage(urls, output);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        
+        src.forEach(Main::download);
+
     }
-    private static String parseFile(String path){
+    private static String parseFile(){
         StringBuilder builder = new StringBuilder();
 
         try {
-            List<String> lines = Files.readAllLines(Paths.get(path));
+            List<String> lines = Files.readAllLines(Paths.get("data/code.html"));
             lines.forEach(line -> builder.append(line + "\n"));
         } catch (IOException e) {
             e.printStackTrace();
         }
         return builder.toString();
     }
-    private static void downloadImage (String src, File path) throws IOException {
-        URL url = new URL(src);
-        FileUtils.copyURLToFile(url, path);
+    public static void download(String src){
+        try (InputStream in = new URL(src).openStream()) {
+            String fileName = src.substring(src.lastIndexOf('/'));
+            Files.copy(in, Paths.get(output + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
 }
