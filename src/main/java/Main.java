78166e93c443e73d8d42d2654650551739e5b769
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-02-14 13:41:03 +0300

    final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,42 +9,43 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
-
 
 public class Main {
 
     public static  String output = "data/image/";
     public static File outFile = new File("data/image");
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        String pathHtml = parseFile();
-
-        Document doc = Jsoup.parse(pathHtml);
+        Document doc = Jsoup.connect("https://lenta.ru/").maxBodySize(0).get();
         Elements img = doc.getElementsByTag("img");
         ArrayList<String> src = new ArrayList<>();
         img.forEach(element -> src.add(element.absUrl("src")));
         src.forEach(Main::download);
 
     }
-    private static String parseFile(){
-        StringBuilder builder = new StringBuilder();
 
-        try {
-            List<String> lines = Files.readAllLines(Paths.get("data/code.html"));
-            lines.forEach(line -> builder.append(line + "\n"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return builder.toString();
-    }
     public static void download(String src){
         try (InputStream in = new URL(src).openStream()) {
-            String fileName = src.substring(src.lastIndexOf('/'));
-            Files.copy(in, Paths.get(output + fileName));
+            String fileName = src.substring(src.lastIndexOf('/')+ 1).replace("?", "");
+            ArrayList<String> imageName = checkImage();
+            if (imageName.contains(fileName)){
+                System.out.println("File " + fileName + " already exists");
+            }else if (!fileName.contains(".jpg") && !fileName.contains(".png")){
+                System.out.println("File " + fileName + " has an unknown format");
+            }else{
+                Files.copy(in, Paths.get(output + fileName));
+                System.out.println("File " + fileName + " uploaded successfully. Directory : " + outFile);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    public static ArrayList<String> checkImage(){
+        ArrayList<String> imageName = new ArrayList<>();
+        for (File item : outFile.listFiles()){
+            imageName.add(item.getName());
+        }
+        return imageName;
     }
 }
